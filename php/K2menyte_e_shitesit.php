f7f148ed4ff8c5974a8c905d5e4da4904711730f
AlbinaKuleta <116918514+AlbinaKuleta@users.noreply.github.com>
2023-05-13 18:01:36 +0200

    kerkesa2-jo perfundimtare

---
+++ php/K2menyte_e_shitesit.php
@@ -0,0 +1,14 @@
+<?php
+// Kontrollo rolin e përdoruesit
+if ($roli == 'profesor') {
+    ?>
+    <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#">Dorëzimet</a></li>
+        <li><a href="#">Vlerësimet</a></li>
+        <li><a href="#">FAQ</a></li>
+    </ul>
+    <?php
+}
+?>
+//vazhdon--prove
