05fc2c8554327cc0a55e0fd64fa61aff46266b37
Diona Muçiqi <diona.muciqi@student.uni-pr.edu>
2023-05-20 09:07:48 +0200

    now is okay

--- html/login_form.php
+++ html/login_form.php
@@ -1,37 +1,42 @@
 <?php
 
-include 'config.php';
+@include 'config.php';
 
 session_start();
 
 if(isset($_POST['submit'])){
-   $name = $_POST['name'];
-   $email = $_POST['email'];
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = md5($_POST['password']);
    $cpass = md5($_POST['cpassword']);
    $user_type = $_POST['user_type'];
 
-   $select = "SELECT * FROM user_form WHERE email = :email AND password = :pass";
-   $stmt = $conn->prepare($select);
-   $stmt->bindParam(':email', $email);
-   $stmt->bindParam(':pass', $pass);
-   $stmt->execute();
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
 
-   if($stmt->rowCount() > 0){
-      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
 
       if($row['user_type'] == 'admin'){
-         $_SESSION['admin_name'] = $row['name'];
-         header('location:manager_page.php');
-      }elseif($row['user_type'] == 'user'){
-         $_SESSION['user_name'] = $row['name'];
-         header('location:client_form.php');
-      }
-   }else{
-      $error[] = 'Incorrect email or password!';
-   }
-}
 
+         $_SESSION['admin_name'] = $row['name'];
+         header('location:admin_page.php');
+
+      }elseif($row['user_type'] == 'user'){
+
+         $_SESSION['user_name'] = $row['name'];
+         header('location:user_page.php');
+
+      }
+     
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+
+};
 ?>
 
 <!DOCTYPE html>
@@ -43,19 +48,19 @@ if(isset($_POST['submit'])){
    <title>Login form</title>
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="/U2/UEB20_23_Gr20/css/style.css">
+   <link rel="stylesheet" href="/ProjektiUeb2/UEB20_23_Gr20/css/style.css">
 
 </head>
 <body>
-
+   
 <div class="form-container">
    <h1>Welcome to Organica</h1>
    <form action="" method="post">
       <h3>Login now</h3>
       <?php
       if(isset($error)){
-         foreach($error as $errorMsg){
-            echo '<span class="error-msg">'.$errorMsg.'</span>';
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
          }
       }
       ?>
