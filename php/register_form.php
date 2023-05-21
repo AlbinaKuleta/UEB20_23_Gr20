e15a859276ede66fc389eed5cf748a388a632bd0
Diona Muçiqi <diona.muciqi@student.uni-pr.edu>
2023-05-21 15:34:25 +0200

    the 6-th request has completed

--- php/register_form.php
+++ php/register_form.php
@@ -1,78 +1,83 @@
 <?php
+// Start session
+session_start();
 
-include 'config.php';
-
-if(isset($_POST['submit'])){
-   $name = $_POST['name'];
-   $email = $_POST['email'];
-   $cemail = $_POST['cemail'];
-   $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   $user_type = $_POST['user_type'];
-
-   $select = "SELECT * FROM user_form WHERE email = :email AND password = :pass";
-   $stmt = $conn->prepare($select);
-   $stmt->bindParam(':email', $email);
-   $stmt->bindParam(':pass', $pass);
-   $stmt->execute();
-
-   if($stmt->rowCount() > 0){
-      $error[] = 'User already exists!';
-   }else{
-      if($pass != $cpass){
-         $error[] = 'Passwords do not match!';
-      }else{
-         $insert = "INSERT INTO user_form (name, email, password, user_type) VALUES (:name, :email, :pass, :user_type)";
-         $stmt = $conn->prepare($insert);
-         $stmt->bindParam(':name', $name);
-         $stmt->bindParam(':email', $email);
-         $stmt->bindParam(':pass', $pass);
-         $stmt->bindParam(':user_type', $user_type);
-         $stmt->execute();
-         header('location: login_form.php');
-      }
-   }
+// Check if user is logged in
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    // Redirect to login page if not logged in
+    header("location: login_form.php");
+    exit;
 }
 
+// Process login form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    // Validate login credentials
+    // Replace with your actual login validation code
+    $username = $_POST['login_username'];
+    $password = $_POST['login_password'];
+    
+    // Check the login credentials against your database or any other authentication mechanism
+    // ...
+    // If login is successful, set session variables
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    
+    // Redirect to profile page
+    header("location: profile.php");
+    exit;
+}
+
+// Process registration form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['register'])) {
+    // Validate and process registration data
+    // Replace with your actual registration code
+    $username = $_POST['register_username'];
+    $password = $_POST['register_password'];
+    
+    // Save the registration data to your database or any other storage mechanism
+    // ...
+    // If registration is successful, set session variables
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    
+    // Redirect to profile page
+    header("location: profile.php");
+    exit;
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>register form</title>
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="/U2/UEB20_23_Gr20/css/style.css">
-
+    <title>User Profile</title>
 </head>
 <body>
+    <h1>Welcome, <?php echo $_SESSION['username']; ?></h1>
 
-<div class="form-container">
-   <form action="" method="post">
-      <h3>register now</h3>
-      <?php
-      if(isset($error)){
-         foreach($error as $errorMsg){
-            echo '<span class="error-msg">'.$errorMsg.'</span>';
-         }
-      }
-      ?>
-      <input type="text" name="name" required placeholder="enter your name">
-      <input type="email" name="email" required placeholder="enter your email">
-      <input type="email" name="cemail" required placeholder="confirm your email">
-      <input type="password" name="password" required placeholder="enter your password">
-      <input type="password" name="cpassword" required placeholder="confirm your password">
-      <select name="user_type">
-         <option value="user">Client</option>
-         <option value="admin">Manager</option>
-      </select>
-      <input type="submit" name="submit" value="register now" class="form-btn">
-      <p>already have an account? <a href="login_form.php">login now</a></p>
-   </form>
-</div>
+    <h2>Profile Information</h2>
+    <!-- Display user profile information here -->
+    <!-- ... -->
 
+    <h2>Login Form</h2>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="login_username">Username:</label>
+        <input type="text" id="login_username" name="login_username" required><br>
+
+        <label for="login_password">Password:</label>
+        <input type="password" id="login_password" name="login_password" required><br>
+
+        <input type="submit" name="login" value="Login">
+    </form>
+
+    <h2>Registration Form</h2>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="register_username">Username:</label>
+        <input type="text" id="register_username" name="register_username" required><br>
+
+        <label for="register_password">Password:</label>
+        <input type="password" id="register_password" name="register_password" required><br>
+
+        <input type="submit" name="register" value="Register">
+    </form>
 </body>
 </html>
